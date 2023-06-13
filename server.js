74c8ef5ce7d255a1e595a58457af7333bac1b48d
Magsud Hajiyev <hajiyev.magsud@fourmeta.com>
2023-06-13 21:03:39 +0400

    Controller ad routing logic added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
 
+const routes = require('./routes');
+
+app.use('/', routes);
+
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {

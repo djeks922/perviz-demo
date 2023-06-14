c22cf9f9053c825829a5eef3460b6125157ce76d
parviz_huseynli <parvizhuseynli@Parvizs-MacBook-Pro.local>
2023-06-14 22:36:27 +0400

    Init

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
-const serverless = require('serverless-http')
 
 const app = express();
 
@@ -18,5 +17,3 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })
-
-module.exports.handler = serverless(app)

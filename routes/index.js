9b3465b9e9da779e771a41e7ad7e6b940259e316
parviz_huseynli <parvizhuseynli@Parvizs-MacBook-Pro.local>
2023-06-14 21:26:22 +0400

    Remove folders

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { makePaymentController } = require('../controllers/paymentController');
-
-router.post('/payment', makePaymentController);
-
-module.exports = router;
